353c31a513c432072e841192076b4c5094811a02
Gohar <goharshatvoryan@gmail.com>
2021-10-09 21:05:26 +0300

    add homework

--- src/homework/FigurePainter.java
+++ src/homework/FigurePainter.java
@@ -20,14 +20,14 @@ public class FigurePainter {
             }
             System.out.println();
         }
-        System.out.println();
+
         for (int i = 4; i >0 ; i--) {
             for (int j = 0; j <i ; j++) {
                 System.out.print("* ");
             }
             System.out.println();
         }
-        System.out.println();
+
 
         for (int i = 4; i >0 ; i--) {
             for (int j = 4; j >i ; j--) {
@@ -39,7 +39,7 @@ public class FigurePainter {
             }
             System.out.println();
         }
-        System.out.println();
+
 
         for (int i = 0; i <4 ; i++) {
             for (int j = 4; j > i; j--) {
